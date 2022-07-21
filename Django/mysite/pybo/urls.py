38e9aae17127b1e4c039d629e9ebcc230d1e8b55
JunYoung Choi <56615819+ekdlakdl12@users.noreply.github.com>
2022-07-21 20:31:42 +0900

    Add answer entry function url mapping complete
    .

--- Django/mysite/pybo/urls.py
+++ Django/mysite/pybo/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'pybo'
+
 urlpatterns =[
-    path('',views.index),
-    path('<int:question_id>/',views.detail), #db테이블 id / 내용 매핑
+    path('',views.index, name='index'), #name값은 프로그램수정시의 번거로움을 제거하기위함
+    path('<int:question_id>/',views.detail, name='detail'), #db테이블 id / 내용 매핑
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 
 ]
 
--- Django/mysite/pybo/views.py
+++ Django/mysite/pybo/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from .models import Question #db임포트
-from django.shortcuts import render, get_object_or_404  # render 함수는 파이썬 데이터를 템플릿에 적용한 HTML을 반환하는 함수이다.
-
+from django.shortcuts import render, get_object_or_404, redirect  # render 함수는 파이썬 데이터를 템플릿에 적용한 HTML을 반환하는 함수이다.
+from django.utils import  timezone
 
 def index(request): #request값이 사용되지 않아 밑줄나옴
     """
@@ -14,7 +14,20 @@ def index(request): #request값이 사용되지 않아 밑줄나옴
 def detail(request, question_id):
     """
     pybo 내용 출력
-    """
+
     question = get_object_or_404(Question, pk=question_id)
     context = {'question' : question}
     return render(request, 'pybo/question_detail.html', context)
+    """
+    question = get_object_or_404(Question,pk=question_id) #id값을 얻어와서 question에 넣음 / 값없을시 404에러
+    context = {'question':question}
+    return render(request,'pybo/question_detail.html',context) #detail html에 저장된 context를 보냄
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('pybo:detail', question_id=question_id)
+
