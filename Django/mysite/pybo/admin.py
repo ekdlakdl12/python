791575044013527807b85849935f754fc5eaad96
JunYoung Choi <56615819+ekdlakdl12@users.noreply.github.com>
2022-10-14 21:26:27 +0900

    Delete Django/mysite directory

--- Django/mysite/pybo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from.models import Question #Question 모델 추가
-# Register your models here.
-
-class QuestionAdmin(admin.ModelAdmin):
-    search_fields = ['subject']
-
-admin.site.register(Question,QuestionAdmin) #키워드 검색 부분
-
