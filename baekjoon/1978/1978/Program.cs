7cab56d4bfcffcde51082ee9cc5e4e3a2f4805dd
JunYeong Choi <56615819+ekdlakdl12@users.noreply.github.com>
2023-11-01 17:35:47 +0900

    Add files via upload

---
+++ baekjoon/1978/1978/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//주어진 수 N개 중에서 소수가 몇 개인지 찾아서 출력하는 프로그램을 작성하시오.
+//첫 줄에 수의 개수 N이 주어진다. N은 100이하이다. 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
+
+//4                  3개
+//1 3 5 7
+
+namespace _1978
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            while (true) 
+            {
+               
+            }
+        }
+    }
+}
